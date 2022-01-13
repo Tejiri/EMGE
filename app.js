e7891da8248399ba0c5578ae1c0b323f84d9479a
Tejiri Emoghene-Ijatomi <steveijatomi@gmail.com>
2022-01-13 14:25:11 +0100

    changes

--- app.js
+++ app.js
@@ -13,14 +13,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "/client/public/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
+  app.use(express.static("client/build"));
 });
 
 
