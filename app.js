78c19358d147893d22cde1eae338cac56ca74874
Tejiri Emoghene-Ijatomi <steveijatomi@gmail.com>
2022-01-13 13:50:10 +0100

    changes

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", routes);
 
 app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "client/public/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+  // res.sendFile(
+  //   path.join(__dirname, "client/public/index.html"),
+  //   function (err) {
+  //     if (err) {
+  //       res.status(500).send(err);
+  //     }
+  //   }
+  // );
 });
 
 const port = process.env.PORT || 8080;
@@ -27,8 +30,6 @@ mongoose.connect(
   }
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
